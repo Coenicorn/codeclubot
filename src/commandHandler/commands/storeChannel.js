352531b5cfbb1b01f518fe60da92b63c71253b9a
Coenicorn <71347039+Coenicorn@users.noreply.github.com>
2022-06-11 10:48:11 +0200

    Delete src/commandHandler/commands directory

--- src/commandHandler/commands/storeChannel.js
+++
@@ -1,9 +0,0 @@
-function onCommand(message) {
-    // do some magic
-}
-
-module.exports = {
-    name: "storeChannel",
-    description: "Stores the channel in a 'database' for future use. (Database being a json file on the server -_-)",
-    run: onCommand
-}
--- src/commandHandler/commands/test.js
+++
@@ -1,9 +0,0 @@
-async function onCommand(message) {
-    message.channel.send("Test succes!");
-}
-
-module.exports = {
-    name: "test",
-    description: "A test command for development purposes!",
-    run: onCommand
-}
